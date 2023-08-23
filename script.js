2c7a8341e0978ac0c4910af739e7b5b7150307ae
Aenon || Noni <114278519+Srmntnn@users.noreply.github.com>
2023-08-24 01:48:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -79,5 +79,25 @@ lightbox.addEventListener('click', e => {
 })
 
 
-//Lightbox for gallery
+//Messenger
+<!-- Messenger Chat Plugin Code -->
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "115662376721603");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+    <!-- Your SDK code -->
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v17.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
 
